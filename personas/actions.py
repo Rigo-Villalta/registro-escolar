35fbee1bcc09747ae9dacdbcbb4f6c83654de800
Rigo-Villalta <40904876+Rigo-Villalta@users.noreply.github.com>
2021-02-03 18:03:34 -0600

    Acciones de excel en estudiantes

--- personas/actions.py
+++ personas/actions.py
@@ -1,80 +1,168 @@
 import codecs
 import csv
+import datetime
+from tempfile import NamedTemporaryFile
 
 from django.http import HttpResponse
-
-
-def exportar_a_excel(self, request, queryset):
-    meta = self.model._meta
-    field_names = [field.name for field in meta.fields]
-
-    response = HttpResponse(content_type='text/csv')
-    response['Content-Disposition'] = 'attachment; filename={}.csv'.format(
-        meta)
-    writer = csv.writer(response)
-
-    response.write(codecs.BOM_UTF8)
-    writer.writerow(field_names)
-    for obj in queryset:
-        row = writer.writerow([getattr(obj, field)
-                               for field in field_names])
-
-    return response
-
-
-def export_as_csv_action(description="Export selected objects as CSV file", fields=None, exclude=None, header=True):
-    """
-    This function returns an export csv action
-    'fields' and 'exclude' work like in django ModelForm
-    'header' is whether or not to output the column names as the first row
-    """
-    def export_as_csv(modeladmin, request, queryset):
-        """
-        Generic csv export admin action.
-        based on http://djangosnippets.org/snippets/1697/
-        """
-        opts = modeladmin.model._meta
-        field_names = set([field.name for field in opts.fields])
-
-        if fields:
-            fieldset = set(fields)
-            field_names = field_names & fieldset
-
-        elif exclude:
-            excludeset = set(exclude)
-            field_names = field_names - excludeset
-
-        response = HttpResponse(content_type='text/csv')
-        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(
-            opts)
-
-        writer = csv.writer(response)
-        response.write(codecs.BOM_UTF8)
-        if header:
-            writer.writerow(list(field_names))
-        for obj in queryset:
-            writer.writerow([getattr(obj, field)
-                             for field in field_names])
-
-        return response
-
-    export_as_csv.short_description = description
-    return export_as_csv
+from openpyxl import Workbook
 
 
 def exportar_a_excel_completo(self, request, queryset):
     meta = self.model._meta
     field_names = [field.name for field in meta.fields]
 
-    response = HttpResponse(content_type='text/csv')
-    response['Content-Disposition'] = 'attachment; filename={}.csv'.format(
-        meta)
+    response = HttpResponse(content_type="text/csv")
+    response["Content-Disposition"] = "attachment; filename={}.csv".format(meta)
     writer = csv.writer(response)
 
     response.write(codecs.BOM_UTF8)
     writer.writerow(field_names)
     for obj in queryset:
-        row = writer.writerow([getattr(obj, field)
-                               for field in field_names])
+        row = writer.writerow([getattr(obj, field) for field in field_names])
 
     return response
+
+
+def exportar_todos_los_datos_a_excel(self, request, queryset):
+    meta = self.model._meta
+    field_names = [field.name for field in meta.fields]
+
+    wb = Workbook()
+    ws = wb.active
+    ws.title = "Estudiantes - Datos completos"
+
+    ws.append(field_names)
+
+    for obj in queryset:
+        ws.append([str(getattr(obj, field)) for field in field_names])
+
+    with NamedTemporaryFile() as tmp:
+        wb.save(tmp.name)
+        tmp.seek(0)
+        stream = tmp.read()
+
+        response = HttpResponse(
+            content=stream,
+            content_type="application/ms-excel",
+        )
+        response[
+            "Content-Disposition"
+        ] = f'attachment; filename=DatosCompletosDeEstudiantes-{datetime.datetime.now().strftime("%Y%m%d%H%M")}.xlsx'
+        return response
+
+
+def exportar_datos_de_contacto_a_excel(self, request, queryset):
+
+    wb = Workbook()
+    ws = wb.active
+    ws.title = "Estudiantes - contactos"
+    ws.append(
+        [
+            "Apellidos",
+            "Nombres",
+            "Sexo",
+            "Edad",
+            "Sección",
+            "Teléfono 1",
+            "Teléfono 2",
+            "Correo Electrónico",
+            "Nombre de responsable",
+            "Relación",
+            "DUI",
+            "Teléfono 1",
+            "Teléfono 2",
+            "Correo Electrónico",
+        ]
+    )
+    for obj in queryset:
+        responsable = obj.responsable
+        if responsable is None:
+            ws.append(
+                [
+                    obj.apellidos,
+                    obj.nombre,
+                    obj.sexo,
+                    obj.edad,
+                    obj.seccion.__str__().title(),
+                    obj.telefono_1,
+                    obj.telefono_2,
+                    obj.correo_electronico,
+                ]
+            )
+        else:
+            ws.append(
+                [
+                    obj.apellidos,
+                    obj.nombre,
+                    obj.sexo,
+                    obj.edad,
+                    obj.seccion.__str__().title(),
+                    obj.telefono_1,
+                    obj.telefono_2,
+                    obj.correo_electronico,
+                    responsable.__str__(),
+                    obj.get_relacion_de_responsable_display(),
+                    responsable.dui,
+                    responsable.telefono_1,
+                    responsable.telefono_2,
+                    responsable.correo_electronico,
+                ]
+            )
+
+    with NamedTemporaryFile() as tmp:
+        wb.save(tmp.name)
+        tmp.seek(0)
+        stream = tmp.read()
+
+        response = HttpResponse(
+            content=stream,
+            content_type="application/ms-excel",
+        )
+        response[
+            "Content-Disposition"
+        ] = f'attachment; filename=DatosDeContactoDeEstudiantes-{datetime.datetime.now().strftime("%Y%m%d%H%M")}.xlsx'
+        return response
+
+
+def exportar_datos_basicos_a_excel(self, request, queryset):
+
+    wb = Workbook()
+    ws = wb.active
+    ws.title = "Estudiantes - Datos básicos"
+    ws.append(
+        [
+            "Apellidos",
+            "Nombres",
+            "Sexo",
+            "Edad",
+            "Sección",
+            "Teléfono 1",
+            "Correo Electrónico",
+        ]
+    )
+    for obj in queryset:
+        ws.append(
+            [
+                obj.apellidos,
+                obj.nombre,
+                obj.sexo,
+                obj.edad,
+                obj.seccion.__str__().title(),
+                obj.telefono_1,
+                obj.correo_electronico,
+            ]
+        )
+
+    with NamedTemporaryFile() as tmp:
+        wb.save(tmp.name)
+        tmp.seek(0)
+        stream = tmp.read()
+
+        response = HttpResponse(
+            content=stream,
+            content_type="application/ms-excel",
+        )
+        response[
+            "Content-Disposition"
+        ] = f'attachment; filename=ListaDeEstudiantes-{datetime.datetime.now().strftime("%Y%m%d%H%M")}.xlsx'
+        return response
--- personas/admin.py
+++ personas/admin.py
@@ -4,9 +4,19 @@ import csv
 from django.contrib import admin
 from django.http import HttpResponse
 
-from django_admin_listfilter_dropdown.filters import DropdownFilter, ChoiceDropdownFilter, RelatedDropdownFilter
+from django_admin_listfilter_dropdown.filters import (
+    DropdownFilter,
+    ChoiceDropdownFilter,
+    RelatedDropdownFilter,
+)
 
-from .actions import export_as_csv_action, exportar_a_excel_completo
+from .actions import (
+    exportar_a_excel_completo,
+    exportar_datos_de_contacto_a_excel,
+    exportar_datos_basicos_a_excel,
+    exportar_todos_los_datos_a_excel
+)
+
 from .models import (
     Departamento,
     Estudiante,
@@ -24,7 +34,9 @@ class FamiliaAdmin(admin.ModelAdmin):
 
 class FamiliaAdminInline(admin.TabularInline):
     model = Familia
-    autocomplete_fields = ['familiar', ]
+    autocomplete_fields = [
+        "familiar",
+    ]
     extra = 0
 
 
@@ -40,7 +52,7 @@ class EstudianteInline(admin.StackedInline):
 
     def has_delete_permission(self, request, obj):
         return False
-    
+
     def has_add_permission(self, request, obj):
         return False
 
@@ -48,7 +60,9 @@ class EstudianteInline(admin.StackedInline):
 class ResponsableAdmin(admin.ModelAdmin):
     search_fields = ["nombre", "apellidos"]
     list_display = ["__str__", "dui"]
-    inlines = [EstudianteInline,]
+    inlines = [
+        EstudianteInline,
+    ]
 
 
 class MenorDeEdadAdmin(admin.ModelAdmin):
@@ -63,79 +77,152 @@ class EstudiantesMenoresAdminInline(admin.TabularInline):
 
 class EstudianteAdmin(admin.ModelAdmin):
     list_display = ["__str__", "grado_matriculado", "seccion", "edad", "sexo"]
-    ordering = ["grado_matriculado__edad_normal_de_ingreso", "seccion", "apellidos", "nombre"]
-    list_filter =  (
+    ordering = [
+        "grado_matriculado__edad_normal_de_ingreso",
+        "seccion",
+        "apellidos",
+        "nombre",
+    ]
+    list_filter = (
         ("sexo", ChoiceDropdownFilter),
         ("grado_matriculado", RelatedDropdownFilter),
-        ("seccion", RelatedDropdownFilter)
-
+        ("seccion", RelatedDropdownFilter),
     )
-    #["grado_matriculado", "seccion"]
+    # ["grado_matriculado", "seccion"]
     search_fields = ["nombre", "apellidos"]
     autocomplete_fields = [
-        "municipio_de_residencia", "municipio_de_nacimiento", "escuela_previa",
-        "familiares", "estudiantes_en_la_misma_casa",
-        "responsable", "menores_cohabitantes"]
+        "municipio_de_residencia",
+        "municipio_de_nacimiento",
+        "escuela_previa",
+        "familiares",
+        "estudiantes_en_la_misma_casa",
+        "responsable",
+        "menores_cohabitantes",
+    ]
     inlines = [EstudiantesMenoresAdminInline, FamiliaAdminInline]
     fieldsets = (
-        ("Datos básicos del estudiante", {
-            'fields': [
-                "nombre", "apellidos", "sexo",
-                "posee_partida", "fecha_de_nacimiento",
-                "nacionalidad", "municipio_de_nacimiento", "estado_civil", ]
-        }),
-        ("Información de contacto", {
-            'fields': [
-                "telefono_1", "telefono_2", "correo_electronico",
-                "municipio_de_residencia", "direccion_de_residencia"]
-        }),
-        ("Información escolar", {
-            'fields': [
-                "nie", "escuela_previa", "seccion_previa",
-                "grado_matriculado", "seccion"]
-        }),
-        ("Información complementaria", {
-            'fields': [
-                "trabaja", "dependencia_economica",  "etnia", "roommate", ]
-        }),
-        ("Información sobre movilidad ciudadana", {
-            'fields': [
-                "utiliza_vehiculo",
-                "utiliza_transporte_publico", "camina_a_la_escuela",
-                "otro_medio_de_transporte", "distancia", ]
-        }),
-        ("Información de salud, seleccione cualquier condición que padezca el estudiante.", {
-            'fields': [
-                "es_autista", "sordera", "ceguera", "baja_vision",
-                "discapacidad_motora", "discapacidad_intelectual",
-                "embarazo", "down", "otro",
-                "usa_medicamentos", "medicamentos", "tipo_de_sangre"
-            ]
-        }),
-        ("Servicios básicos y comunicaciones.", {
-            'fields': [
-                "fuente_de_agua", "piso", "tipo_de_sanitario",
-                "posee_energia", "posee_refrigerador", "posee_televisor",
-                "posee_celular_con_acceso_a_internet",
-                "posee_radio", "posee_computadora", "posee_tablet",
-                "internet_residencial", "compania_internet",
-            ]
-        }),
-        ("Situación familiar", {
-            'fields': [
-                "cantidad_cohabitantes", "menores_en_casa",
-                "responsable", "relacion_de_responsable", "estudiantes_en_la_misma_casa", ]
-        }),
+        (
+            "Datos básicos del estudiante",
+            {
+                "fields": [
+                    "nombre",
+                    "apellidos",
+                    "sexo",
+                    "posee_partida",
+                    "fecha_de_nacimiento",
+                    "nacionalidad",
+                    "municipio_de_nacimiento",
+                    "estado_civil",
+                ]
+            },
+        ),
+        (
+            "Información de contacto",
+            {
+                "fields": [
+                    "telefono_1",
+                    "telefono_2",
+                    "correo_electronico",
+                    "municipio_de_residencia",
+                    "direccion_de_residencia",
+                ]
+            },
+        ),
+        (
+            "Información escolar",
+            {
+                "fields": [
+                    "nie",
+                    "escuela_previa",
+                    "seccion_previa",
+                    "grado_matriculado",
+                    "seccion",
+                ]
+            },
+        ),
+        (
+            "Información complementaria",
+            {
+                "fields": [
+                    "trabaja",
+                    "dependencia_economica",
+                    "etnia",
+                    "roommate",
+                ]
+            },
+        ),
+        (
+            "Información sobre movilidad ciudadana",
+            {
+                "fields": [
+                    "utiliza_vehiculo",
+                    "utiliza_transporte_publico",
+                    "camina_a_la_escuela",
+                    "otro_medio_de_transporte",
+                    "distancia",
+                ]
+            },
+        ),
+        (
+            "Información de salud, seleccione cualquier condición que padezca el estudiante.",
+            {
+                "fields": [
+                    "es_autista",
+                    "sordera",
+                    "ceguera",
+                    "baja_vision",
+                    "discapacidad_motora",
+                    "discapacidad_intelectual",
+                    "embarazo",
+                    "down",
+                    "otro",
+                    "usa_medicamentos",
+                    "medicamentos",
+                    "tipo_de_sangre",
+                ]
+            },
+        ),
+        (
+            "Servicios básicos y comunicaciones.",
+            {
+                "fields": [
+                    "fuente_de_agua",
+                    "piso",
+                    "tipo_de_sanitario",
+                    "posee_energia",
+                    "posee_refrigerador",
+                    "posee_televisor",
+                    "posee_celular_con_acceso_a_internet",
+                    "posee_radio",
+                    "posee_computadora",
+                    "posee_tablet",
+                    "internet_residencial",
+                    "compania_internet",
+                ]
+            },
+        ),
+        (
+            "Situación familiar",
+            {
+                "fields": [
+                    "cantidad_cohabitantes",
+                    "menores_en_casa",
+                    "responsable",
+                    "relacion_de_responsable",
+                    "estudiantes_en_la_misma_casa",
+                ]
+            },
+        ),
     )
-    actions = [export_as_csv_action("Exportar a Excel datos básicos", fields=[
-                                    "nombre", "apellidos", "nie", "seccion", "sexo", "edad"]),
-                                    exportar_a_excel_completo]
+    actions = [exportar_datos_basicos_a_excel, exportar_datos_de_contacto_a_excel, exportar_todos_los_datos_a_excel]
 
+exportar_datos_basicos_a_excel.short_description = "Exportar datos básicos a excel."
 
 
 class MunicipioAdmin(admin.ModelAdmin):
     search_fields = ["nombre"]
     ordering = ["id"]
+
 
 admin.site.register(Departamento)
 admin.site.register(Estudiante, EstudianteAdmin)
