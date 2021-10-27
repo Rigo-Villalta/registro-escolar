6f4bb3fb87836e9ac3f3a5dfd4cbb2717c835f76
Rigo-Villalta <40904876+Rigo-Villalta@users.noreply.github.com>
2021-10-27 11:42:40 -0600

    Cambio de openpyxl a xlsxWriter por rendimiento

--- personas/actions.py
+++ personas/actions.py
@@ -1,4 +1,5 @@
-import datetime
+import datetime, io
+import xlsxwriter
 from tempfile import NamedTemporaryFile
 
 from django.http import HttpResponse
@@ -420,15 +421,11 @@ def exportar_a_excel_lista_de_firmas_por_seccion_y_familia(self, request, querys
     a excel las columnas: Responsable, DUI de responsable,
     estudiante y sección del estudiante, ordenados en primer
     lugar por sección, pero con familias en conjunto, es decir
-    el primer niño de kinder 3 es el primero, si tiene hermanos
-    luego ellos, luego el segundo niño de kinder 3 y sus hermanos y así
+    si le primer nivel es kinder 3, el primer nino de ese grado aparece
+    seguido de sus heramnos, luego el segundo niño de kinder 3 y sus hermanos y así
     sucesivamente, estos hermanos ya no aparecen en su repectiva seccion
-    haciemos uso de la librería openpyxl ver: openpyxl.readthedocs.io
+    haciemos uso de la librería XlsxWriter ver: xlsxwriter.readthedocs.io
     """
-
-    # optimización del queryset para los datos
-    # de responsable relacionados y ordenados
-    # para ello pasamos el query a values
 
     estudiantes = (
         queryset.prefetch_related("grado_matriculado__nivel")
@@ -446,7 +443,17 @@ def exportar_a_excel_lista_de_firmas_por_seccion_y_familia(self, request, querys
         )
     )
 
-    wb = Workbook()
+    output = io.BytesIO()
+    wb = xlsxwriter.Workbook(output)
+    header_format = wb.add_format({"bold": 1, "align": "center", "valign": "vcenter"})
+    list_header_format = wb.add_format(
+        {"bold": 1, "border": 1, "bg_color": "#BBBBBB", "valign": "vcenter"}
+    )
+    list_body_format = wb.add_format(
+        {
+            "border": 1,
+        }
+    )
 
     responsables_usados = []
     count = 0
@@ -458,118 +465,84 @@ def exportar_a_excel_lista_de_firmas_por_seccion_y_familia(self, request, querys
             count += 1
         else:
             if estudiante["seccion__id"] != seccion_actual:
-                if ws is not None:
-                    set_border(ws, "A7:F" + str(counter_interno + 6))
-                    for row in range(8, counter_interno + 7):
-                        rd = ws.row_dimensions[row]
-                        rd.height = 25
-                        a = ws["A" + str(row)]
-                        a.alignment = Alignment(horizontal="center")
-                ws = wb.create_sheet(
-                    f"{estudiante['grado_matriculado__nivel']}{estudiante['seccion__seccion']}"
-                )
-                ws.title = (
+                ws = wb.add_worksheet(
                     f"{estudiante['grado_matriculado__nivel']} {estudiante['seccion__seccion']}".title()
-                    .replace(" ", "")
                     .replace("Años", "")
                     .replace("Año", "")
                     .replace("Bachillerato", "")
                 )
-                ws.column_dimensions["A"].width = 3
-                ws.column_dimensions["B"].width = 36
-                ws.column_dimensions["C"].width = 11
-                ws.column_dimensions["D"].width = 36
-                ws.column_dimensions["E"].width = 40
-                ws.column_dimensions["F"].width = 20
-                ws["A1"] = 'Complejo Educativo "Dr. Humberto Romero Alvergue"'
-                bold = Font(bold=True)
-                a1 = ws["A1"]
-                a1.font = bold
-                a1.alignment = Alignment(horizontal="center")
-                ws.merge_cells("A1:F1")
-                ws["A2"] = "Código de Infraestructura 11674"
-                a2 = ws["A2"]
-                a2.font = bold
-                a2.alignment = Alignment(horizontal="center")
-                ws.merge_cells("A2:F2")
-                ws["A3"] = "Descripción de actividad"
-                ws.merge_cells("A3:F3")
-                ws["A4"] = "Fecha de entrega"
-                ws.merge_cells("A4:F4")
-                ws["A5"] = "Se recibe"
-                ws.merge_cells("A5:F5")
-                ws["A6"] = ""
-                ws.merge_cells("A6:F6")
-                ws.append(
+                ws.set_column(0, 0, 3)
+                ws.set_column(1, 1, 36)
+                ws.set_column(2, 2, 11)
+                ws.set_column(3, 3, 36)
+                ws.set_column(4, 4, 40)
+                ws.set_column(5, 5, 20)
+                ws.merge_range(
+                    "A1:F1",
+                    'Complejo Educativo "Dr. Humberto Romero Alvergue"',
+                    header_format,
+                )
+                ws.merge_range(
+                    "A2:F2", "Código de Infraestructura 11674", header_format
+                )
+                ws.merge_range("A3:F3", "Descripción de actividad")
+                ws.merge_range("A4:F4", "Fecha de entrega")
+                ws.merge_range("A5:F5", "Se recibe")
+                ws.merge_range("A6:F6", "")
+                ws.write_row(
+                    "A7",
                     [
                         "Nº",
-                        "Nombre de Padre /Madre o Responsable",
+                        "Nombre de Responsable",
                         "Nº de DUI",
                         "Estudiante",
                         "Sección de estudiante",
                         "Firma",
-                    ]
+                    ],
+                    list_header_format,
                 )
                 counter_interno = 1
-                a7 = ws["A7"]
-                b7 = ws["B7"]
-                c7 = ws["C7"]
-                d7 = ws["D7"]
-                e7 = ws["E7"]
-                f7 = ws["F7"]
-                fill_gray = PatternFill(
-                    fill_type="solid", start_color="DDDDDD", end_color="DDDDDD"
-                )
-                a7.fill = fill_gray
-                b7.fill = fill_gray
-                c7.fill = fill_gray
-                d7.fill = fill_gray
-                e7.fill = fill_gray
-                f7.fill = fill_gray
-            ws.append(
+            ws.write_row(
+                counter_interno + 6,
+                0,
                 [
                     str(counter_interno),
                     f"{estudiante['responsable__apellidos']}, {estudiante['responsable__nombre']}",
                     estudiante["responsable__dui"],
                     f"{estudiante['apellidos']}, {estudiante['nombre']}",
                     f"{estudiante['grado_matriculado__nivel'].title()} {estudiante['seccion__seccion']}",
-                ]
+                    "",
+                ],
+                list_body_format,
             )
+            ws.set_row(counter_interno + 6, 25)
             counter_interno += 1
             for estudiante_b in estudiantes[count + 1 :]:
                 if estudiante["responsable__id"] == estudiante_b["responsable__id"]:
-                    ws.append(
+                    ws.write_row(
+                        counter_interno + 6,
+                        0,
                         [
                             str(counter_interno),
                             f"{estudiante_b['responsable__apellidos']}, {estudiante_b['responsable__nombre']}",
                             estudiante_b["responsable__dui"],
                             f"{estudiante_b['apellidos']}, {estudiante_b['nombre']}",
                             f"{estudiante_b['grado_matriculado__nivel'].title()} {estudiante_b['seccion__seccion']}",
-                        ]
+                            "",
+                        ],
+                        list_body_format,
                     )
+                    ws.set_row(counter_interno + 6, 25)
                     counter_interno += 1
             seccion_actual = estudiante["seccion__id"]
             responsables_usados.append(estudiante["responsable__id"])
             count += 1
-
-    set_border(ws, "A7:F" + str(counter_interno + 6))
-    for row in range(8, counter_interno + 7):
-        rd = ws.row_dimensions[row]
-        rd.height = 25
-        a = ws["A" + str(row)]
-        a.alignment = Alignment(horizontal="center")
-    del wb["Sheet"]
-
-    with NamedTemporaryFile() as tmp:
-        wb.save(tmp.name)
-        tmp.seek(0)
-        stream = tmp.read()
-
-        response = HttpResponse(
-            content=stream,
-            content_type="application/ms-excel",
-        )
-        response[
-            "Content-Disposition"
-        ] = f'attachment; filename=ListaDeEstudiantesYResponsablesParaFirmas-{datetime.datetime.now().strftime("%Y_%m_%d-%H%M")}.xlsx'
-        return response
+    wb.close()
+    output.seek(0)
+    filename = f"ListaDeEstudiantesYResponsablesParaFirmas-{datetime.datetime.now().strftime('%Y_%m_%d-%H%M')}.xlsx"
+    response = HttpResponse(
+        output,
+        content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    )
+    response["Content-Disposition"] = "attachment; filename=%s" % filename
+    return response
