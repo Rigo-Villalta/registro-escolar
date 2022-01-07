21ca02b7c2b35f8c53ec7f451e2fd8644006b4a4
Rigo-Vilallta <40904876+Rigo-Villalta@users.noreply.github.com>
2022-01-06 21:22:36 -0600

    Agregados métodos de usuario

--- users/models.py
+++ users/models.py
@@ -1,18 +1,24 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
 
-    TIPOS_DE_USUARIO = (
-        (1, "Administrador"),
-        (2, "Profesor"),
-        (3, "Digitador")
-    )
+    TIPOS_DE_USUARIO = ((1, "Administrador"), (2, "Profesor"), (3, "Digitador"))
     tipo_de_usuario = models.PositiveSmallIntegerField(
-        choices=TIPOS_DE_USUARIO,
-        default=3
+        choices=TIPOS_DE_USUARIO, default=3
     )
-    
+
     class Meta:
-        db_table='auth_user'
-        verbose_name= "usuario"
+        db_table = "auth_user"
+        verbose_name = "usuario"
+        verbose_name_plural = "usuarios"
+
+    def es_administrador(self):
+        return self.tipo_de_usuario == 1
+
+    def es_profesor(self):
+        return self.tipo_de_usuario == 2
+
+    def es_digitador(self):
+        return self.tipo_de_usuario == 3
