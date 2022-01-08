9a1d6557a8a445914ebba3bfc23972db8c100677
Rigo-Villalta <40904876+Rigo-Villalta@users.noreply.github.com>
2022-01-08 17:29:36 -0600

    viestas exportar CSV´s removidas por obsoletas

--- escuela/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import index, secciones_csv, grados_csv
-
-urlpatterns = [
-    path('', index),
-    path("secciones_csv", secciones_csv, name="secciones_csv" ),
-    path("grados_csv", grados_csv, name="grados_csv")
-]
