fa87a196fe74a3cc7fa479117b534b823380be89
Rigo-Vilallta <40904876+Rigo-Villalta@users.noreply.github.com>
2022-01-10 14:46:04 -0600

    Filtros para usuario y queryset

--- users/admin.py
+++ users/admin.py
@@ -46,13 +46,19 @@ class CustomUserAdmin(UserAdmin):
 
     list_display = ("username", "email", "last_name", "first_name", "tipo_de_usuario")
     readonly_fields = ("last_login", "date_joined")
-    list_filter = ["groups"]
+    list_filter = ["tipo_de_usuario", "is_active"]
 
     def get_actions(self, request):
         actions = super().get_actions(request)
         if "delete_selected" in actions:
             del actions["delete_selected"]
         return actions
+    
+    def get_queryset(self, request):
+        active_filter = request.GET.get("is_active__exact")
+        if active_filter:
+            return super().get_queryset(request)
+        return super().get_queryset(request).filter(is_active=True)
 
 
 escuela_admin.register(User, CustomUserAdmin)
