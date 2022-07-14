308acffe58d9b12d05e4cf9c55b9280cbeb9c7a4
Rigo-Vilallta <40904876+Rigo-Villalta@users.noreply.github.com>
2022-07-14 14:11:42 -0600

    Texto si no hay faltas de estudiante

--- disciplina/admin.py
+++ disciplina/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
+from escuela.models import PeriodoEscolar
 from escuela.admin import escuela_admin
 
 from .models import Falta, FaltaDisciplinariaEstudiantil
@@ -20,9 +21,23 @@ class FaltaDisciplinariaEstudiantilInline(admin.TabularInline):
     fields = ["falta", "fecha"]
     readonly_fields = ["falta", "fecha"]
     extra = 0
+    show_change_link = True
+    verbose_name_plural = "Faltas disciplinarias cometidas por el estudiante"
+    template = "admin/faltas_de_estudiante_inline.html"
 
     def has_add_permission(self, request, obj):
         return False
+
+    def has_delete_permission(self, request, obj):
+        return False
+
+    def get_queryset(self, request):
+        periodo_escolar_activo = PeriodoEscolar.objects.get(periodo_activo=True)
+        return (
+            super()
+            .get_queryset(request)
+            .filter(fecha__gt=periodo_escolar_activo.fecha_de_inicio)
+        )
 
 
 admin.site.register(Falta, FaltaAdmin)
