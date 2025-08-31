b019319100c965eb6fe98d97390309b665af141c
Rigoberto Villalta <40904876+Rigo-Villalta@users.noreply.github.com>
2025-08-31 11:01:49 -0600

    Removidos tipos de usuarios, retornamos a permisos y grupos de auth

--- users/admin.py
+++ users/admin.py
@@ -10,44 +10,7 @@ class CustomUserAdmin(UserAdmin):
     """
     Usuario admin.
     """
-
-    fieldsets = (
-        (_("user"), {"fields": ("username", "password", "is_active", "is_staff")}),
-        (
-            _("Personal info"),
-            {
-                "fields": (
-                    "first_name",
-                    "last_name",
-                    "tipo_de_usuario",
-                    "email",
-                )
-            },
-        ),
-        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
-    )
-    add_fieldsets = (
-        (
-            None,
-            {
-                "classes": ("wide",),
-                "fields": (
-                    "username",
-                    "email",
-                    "password1",
-                    "password2",
-                    "is_staff",
-                    "first_name",
-                    "last_name",
-                    "tipo_de_usuario",
-                ),
-            },
-        ),
-    )
-
-    list_display = ("username", "email", "last_name", "first_name", "tipo_de_usuario")
-    readonly_fields = ("last_login", "date_joined")
-    list_filter = ["tipo_de_usuario", "is_active"]
+    readonly_fields = ["last_login", "date_joined"]
 
     def get_actions(self, request):
         actions = super().get_actions(request)
--- users/models.py
+++ users/models.py
@@ -1,24 +1,11 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models
 
 
 class User(AbstractUser):
-
-    TIPOS_DE_USUARIO = ((1, "Administrador"), (2, "Profesor"), (3, "Digitador"))
-    tipo_de_usuario = models.PositiveSmallIntegerField(
-        choices=TIPOS_DE_USUARIO, default=3
-    )
-
+    """
+    Usuario extendido para futuras personalizaciones
+    """
     class Meta:
         db_table = "auth_user"
         verbose_name = "usuario"
         verbose_name_plural = "usuarios"
-
-    def es_administrador(self):
-        return self.tipo_de_usuario == 1
-
-    def es_profesor(self):
-        return self.tipo_de_usuario == 2
-
-    def es_digitador(self):
-        return self.tipo_de_usuario == 3
